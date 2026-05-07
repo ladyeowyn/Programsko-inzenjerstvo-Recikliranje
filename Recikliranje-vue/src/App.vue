5ac58a986bd440e52051bf46df95bc69dfaf3489
ladyeowyn <kohutlana@gmail.com>
2026-05-07 15:56:26 +0200

    komponente

--- Recikliranje-vue/src/App.vue
+++ Recikliranje-vue/src/App.vue
@@ -1,7 +1,28 @@
 <script setup>
+import Educirajse from './components/Educirajse.vue';
+import FAQ from './components/FAQ.vue';
 import HelloWorld from './components/HelloWorld.vue'
+import HomeScreen from './components/HomeScreen.vue';
+import Kontejneri_guest from './components/kontejneri_guest.vue';
+import Kontejneri_loggedin from './components/kontejneri_loggedin.vue';
+import Login from './components/Login.vue';
+import Lokacija from './components/Lokacija.vue';
+import Obavijesti from './components/Obavijesti.vue';
+import Reciklazna_dvorista from './components/Reciklazna_dvorista.vue';
+import Registracija from './components/Registracija.vue';
+import Search from './components/Search.vue';
 </script>
 
 <template>
-  <HelloWorld />
+  <HomeScreen />
+  <Educirajse />
+  <FAQ />
+  <Kontejneri_guest />
+  <Kontejneri_loggedin/>
+  <Login/>
+  <Lokacija/>
+  <Obavijesti/>
+  <Reciklazna_dvorista/>
+  <Registracija/>
+  <Search/>
 </template>
